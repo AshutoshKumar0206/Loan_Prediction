87be4a13341b1fdfc1a6b13c066e31f1d7d46fb6
Ashutosh Kumar <ashutoshbokaro2003@gmail.com>
2026-02-11 11:57:26 +0530

    Loan Prediction

--- loan_prediction.py
+++ loan_prediction.py
@@ -7,16 +7,26 @@ df = pd.read_csv("../train_u6lujuX_CVtuZ9i.csv")
 df.dropna(inplace=True)
 
 df = df.drop("Loan_ID", axis=1)
-cat_cols = df.columns
+df["LoanAmount"].fillna(df["LoanAmount"].median(), inplace=True)
+df["Loan_Amount_Term"].fillna(df["Loan_Amount_Term"].median(), inplace=True)
+df["Credit_History"].fillna(df["Credit_History"].mode()[0], inplace=True)
+df["Dependents"] = df["Dependents"].replace("3+", "3")
+df["Dependents"] = df["Dependents"].astype(int)
+for col in ["Gender", "Married", "Self_Employed", "Dependents"]:
+    df[col].fillna(df[col].mode()[0], inplace=True)
+
+# ---- Binary Encoding ----
 df["Gender"] = df['Gender'].map({"Male":1, "Female":0})
 df["Education"] = df['Education'].map({"Graduate":1, "Not Graduate":0})
 df["Married"] = df['Married'].map({"Yes":1, "No":0})
 df["Self_Employed"] = df['Self_Employed'].map({"Yes":1, "No":0})
-df["Property_Area"] = df['Property_Area'].map({"Urban":1, "Rural":0, "Semiurban":2})
 df["Loan_Status"] = df['Loan_Status'].map({"Y":1, "N":0})
+
+# ---- ONE HOT for Property Area ----
+df = pd.get_dummies(df, columns=["Property_Area"], drop_first=True)
 # for col in cat_cols:
 #     print(df[col].value_counts())
-df = df.drop(["Dependents", "Married", "ApplicantIncome"], axis=1)
+# df = df.drop(["Dependents", "Married", "ApplicantIncome"], axis=1)
 # sns.countplot(x="Credit_History", data=df)
 # df["Credit_History"].hist()
 # sns.countplot(x='Credit_History', hue='Loan_Status', data=df)
@@ -28,6 +38,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
 #Model used of logistic Regression
 
 # df[num_cols] = scale.fit_transform(df[num_cols])
@@ -37,14 +48,15 @@ y = df["Loan_Status"]
 num_cols = x.select_dtypes(include=['int64','float64']).columns
 scale = StandardScaler()
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42, stratify=y)
 x_train[num_cols] = scale.fit_transform(x_train[num_cols])
 x_test[num_cols] = scale.transform(x_test[num_cols])
 
 
-model = LogisticRegression(class_weight='balanced')
-# model = GaussianNB()
-#till now naive bayes is better than logistic regression model
+# model = LogisticRegression(class_weight='balanced', max_iter=500)#logistic regression model
+# model = GaussianNB()#naive bayes model
+model = SVC(kernel='linear', class_weight='balanced', C=1.0)
+#till now svc is better than naive bayes and logistic regression model
 model.fit(x_train, y_train)
 
 y_pred = model.predict(x_test)
